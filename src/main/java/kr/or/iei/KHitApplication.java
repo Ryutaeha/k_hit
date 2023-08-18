657dd3b3321e6f0f416dd785b60e2c87780a72fb
Ryutaeha <142005732+Ryutaeha@users.noreply.github.com>
2023-08-18 16:15:36 +0900

    Initial commit

---
+++ src/main/java/kr/or/iei/KHitApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KHitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KHitApplication.class, args);
+	}
+
+}
