ab1643fbd23bc7e71c2876f9b397532165b6fc2e
Ryutaeha <142005732+Ryutaeha@users.noreply.github.com>
2023-08-21 17:03:02 +0900

    기초공사 ver.2

--- src/main/java/kr/or/iei/admin/controller/AdminController.java
+++ src/main/java/kr/or/iei/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package kr.or.iei.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.iei.admin.model.service.AdminService;
@@ -11,4 +12,9 @@ import kr.or.iei.admin.model.service.AdminService;
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
+	
+	@GetMapping(value = "/adminIndex")
+	public String adminIndex() {
+		return "/admin/adminIndex";
+	}
 }
