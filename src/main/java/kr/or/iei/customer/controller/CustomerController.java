e642e2e6ac6d33c9668272d5169d09417cb45475
u1wns <127290079+u1wns@users.noreply.github.com>
2023-08-22 15:19:00 +0900

    장바구니 - 남수민

--- src/main/java/kr/or/iei/customer/controller/CustomerController.java
+++ src/main/java/kr/or/iei/customer/controller/CustomerController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-
+	//회원가입
 	@GetMapping(value = "/join")
 	public String customerJoin() {
 		return "/customer/join";
+	}
+	//장바구니
+	@GetMapping(value="/cart")
+	public String customerCart() {
+		return "/customer/cart";
 	}
 }
