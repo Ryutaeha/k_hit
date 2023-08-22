2f944b571d0d6175ec592642e9622c38ac08f715
kyungsik1101 <142005877+kyungsik1101@users.noreply.github.com>
2023-08-22 20:40:14 +0900

    판매자회원가입_김경식

--- src/main/java/kr/or/iei/seller/controller/SellerController.java
+++ src/main/java/kr/or/iei/seller/controller/SellerController.java
@@ -3,6 +3,7 @@ package kr.or.iei.seller.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.iei.seller.model.service.SellerService;
@@ -11,14 +12,7 @@ import kr.or.iei.seller.model.service.SellerService;
 @RequestMapping(value="/seller")
 public class SellerController {
 	@Autowired
-	private SellerService sellerService;
-	
-	/*
-	@GetMapping(value="/sellerMenu")
-	public String sellerMenu() {
-		return "/seller/sellerMenu";
-	}
-	*/
+	private SellerService sellerService;	
 	
 	@GetMapping(value="/productManagement")
 	public String productManagement() {
@@ -28,5 +22,15 @@ public class SellerController {
 	@GetMapping(value="/addNewProduct")
 	public String addNewProduct() {
 		return "/seller/addNewProduct";
+	}
+	//판매자 회원가입
+	@GetMapping(value="/join")
+	public String joinPage() {
+		return "/seller/join";
+	}
+	//회원가입 완료
+	@PostMapping(value="/joinComplete")
+	public String joinComplete() {
+		return "/seller/joinComplete";
 	}
 }
