ad88d93e318d0c8f896a9d4cb0da9d1891ac19b7
u1wns <127290079+u1wns@users.noreply.github.com>
2023-08-23 18:02:19 +0900

    Update CustomerController.java

--- src/main/java/kr/or/iei/customer/controller/CustomerController.java
+++ src/main/java/kr/or/iei/customer/controller/CustomerController.java
@@ -23,16 +23,11 @@ public class CustomerController {
 	public String customerJoin() {
 		return "customer/join";
 	}
-	//장바구니
+	//장바구니 페이지
 	@GetMapping(value="/cart")
-	public String customerCart() {
+	public String customerGetCart() {
 		return "/customer/cart";
 	}
-	//결제하기
-		@GetMapping(value="/payment")
-		public String customerPayment() {
-			return "/customer/payment";
-		}
 
 
 	//회원가입
