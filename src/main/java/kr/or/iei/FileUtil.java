7ccf519788de490eaba6e55276eb46de51db9b83
Mojitokyk <over100s@naver.com>
2023-08-25 16:51:51 +0900

    리뷰 회원가입 로그인 css 수정

--- src/main/java/kr/or/iei/FileUtil.java
+++ src/main/java/kr/or/iei/FileUtil.java
@@ -1,6 +1,14 @@
 package kr.or.iei;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 
@@ -36,5 +44,48 @@ public class FileUtil {
 			count++;
 		}
 		return filepath;
+	}
+	public void downloadFile(String savepath, String filename, String filepath, HttpServletResponse response) {
+		String downFile = savepath+filepath;
+		
+		try {
+			//파일을 객체로 읽어오기 위한 스트림 생성
+			FileInputStream fis = new FileInputStream(downFile);
+			//속도개선을 위한 보조스크림 생성
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			
+			//읽어온 파일을 사용자에게 내보낼 스트림 생성
+			ServletOutputStream sos = response.getOutputStream();
+			//속도개선을 위한 보조스트림 생성
+			BufferedOutputStream bos = new BufferedOutputStream(sos);
+			
+			//파일명처리
+			String resFilename = new String(filename.getBytes("UTF-8"),"ISO-8859-1");
+			
+			//파일 다운로드를 위한 HTTP 헤더 설정
+			//파일이라는 것을 알려줌
+			response.setContentType("application/octet-stream");
+			//주는 파일이 무엇인지 알려줌
+			response.setHeader("Content-Disposition", "attachment;filename="+resFilename);
+			
+			//파일을 읽어온 뒤 전송
+			while(true) {
+				int data = bis.read();
+				if(data != -1) {
+					bos.write(data);
+				}else {
+					break;
+				}
+			}
+			bis.close();
+			bos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- src/main/java/kr/or/iei/review/controller/ReviewController.java
+++ src/main/java/kr/or/iei/review/controller/ReviewController.java
@@ -1,14 +1,19 @@
 package kr.or.iei.review.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.multipart.MultipartFile;
 
-import kr.or.iei.customer.model.service.CustomerService;
+import kr.or.iei.FileUtil;
 import kr.or.iei.customer.model.vo.Customer;
 import kr.or.iei.review.model.service.ReviewService;
 import kr.or.iei.review.model.vo.Review;
@@ -18,21 +23,31 @@ import kr.or.iei.review.model.vo.Review;
 public class ReviewController {
 	@Autowired 
 	private ReviewService reviewService;
-	@Autowired 
-	private CustomerService customerService;
+	@Autowired
+	private FileUtil fileUtil;
+	@Value("${file.root}")
+	private String root;
 	
 	//리뷰 작성/수정 페이지 이동
 	@GetMapping("/reviewWriteFrm") //주문내역 넘어오는 것 보고 수정하기
-	public String reviewWrtieFrm(@SessionAttribute(required = false) Customer c,String orderNo, Model model) {
-		model.addAttribute("reviewWriter", c.getCustomerId());
-		model.addAttribute("orderNo",orderNo);
+	public String reviewWrtieFrm(Model model) {
+		
 		return "review/reviewWriteFrm";
 	}
 	//리뷰 작성 
-	@GetMapping("/reviewWrite")
-	public String reviewWrtie(Review review, Model model) {
-		//파일 넘기는 것 추가해야됨
-		int result = reviewService.insertReview(review);
+	@PostMapping("/reviewWrite")
+	public String reviewWrtie(Review r,MultipartFile imageFile, Model model) {
+		String savepath = root+"photo/";
+		String filepath = fileUtil.getFilepath(savepath, imageFile.getOriginalFilename());
+		r.setFilepath(filepath);
+		File upFile = new File(savepath+filepath);
+		try {
+			imageFile.transferTo(upFile);
+		} catch (IllegalStateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int result = reviewService.insertReview(r);
 		if(result>0) {
 			model.addAttribute("title","REVIEW");
 			model.addAttribute("msg", "후기 등록 완료");
