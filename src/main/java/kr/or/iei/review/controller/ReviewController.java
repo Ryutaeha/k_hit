2075806742dad0bb47a0571da8687c6b33ce4199
Mojitokyk <over100s@naver.com>
2023-08-25 14:50:54 +0900

    리뷰추가

--- src/main/java/kr/or/iei/review/controller/ReviewController.java
+++ src/main/java/kr/or/iei/review/controller/ReviewController.java
@@ -2,26 +2,62 @@ package kr.or.iei.review.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import kr.or.iei.customer.model.service.CustomerService;
+import kr.or.iei.customer.model.vo.Customer;
+import kr.or.iei.review.model.service.ReviewService;
+import kr.or.iei.review.model.vo.Review;
 
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
 	@Autowired 
+	private ReviewService reviewService;
+	@Autowired 
 	private CustomerService customerService;
 	
-	//리뷰 작성/수정
+	//리뷰 작성/수정 페이지 이동
+	@GetMapping("/reviewWriteFrm") //주문내역 넘어오는 것 보고 수정하기
+	public String reviewWrtieFrm(@SessionAttribute(required = false) Customer c,String orderNo, Model model) {
+		model.addAttribute("reviewWriter", c.getCustomerId());
+		model.addAttribute("orderNo",orderNo);
+		return "review/reviewWriteFrm";
+	}
+	//리뷰 작성 
 	@GetMapping("/reviewWrite")
-	public String reviewWrtie() {
-		return "review/reviewWriteFrm"; //경로 나중에 수정하기
+	public String reviewWrtie(Review review, Model model) {
+		//파일 넘기는 것 추가해야됨
+		int result = reviewService.insertReview(review);
+		if(result>0) {
+			model.addAttribute("title","REVIEW");
+			model.addAttribute("msg", "후기 등록 완료");
+			model.addAttribute("icon", "success");
+			model.addAttribute("loc", "/review/reviewView");
+		}else {
+			model.addAttribute("title","REVIEW");
+			model.addAttribute("msg", "후기 등록 실패");
+			model.addAttribute("icon", "error");
+			model.addAttribute("loc", "/review/reviewWriteFrm");
+		}
+		return "common/msg";
 	}
 	
 	//리뷰 상세 보기
 	@GetMapping("/reviewView")
 	public String reviewVeiw() {
+		
 		return "review/reviewView";//경로 나중에 수정하기
 	}
+	
+	//리뷰게시판
+	@GetMapping("/reviewList")
+	public String reviewList() {
+		return "review/reviewList";
+	}
+	
 }
--- src/main/java/kr/or/iei/review/model/service/ReviewService.java
+++ src/main/java/kr/or/iei/review/model/service/ReviewService.java
@@ -1,5 +1,20 @@
 package kr.or.iei.review.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.iei.review.model.dao.ReviewDao;
+import kr.or.iei.review.model.vo.Review;
+
+@Service
 public class ReviewService {
+	@Autowired
+	private ReviewDao reviewDao;
+	
+	//리뷰 등록
+	public int insertReview(Review review) {
+		int result = reviewDao.insertReview(review);
+		return result;
+	}
 
 }
